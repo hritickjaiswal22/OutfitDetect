82d676fe908f72a42c7da91f421c44443f2b1ef1
newbiehritick <newbiehritick@gmail.com>
2021-06-16 19:48:41 +0530

    first commit

---
+++ Outfit detect/gad.py
@@ -0,0 +1,140 @@
+import cv2
+import math
+import argparse
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
+
+def highlightFace(net, frame, conf_threshold=0.7):
+    frameOpencvDnn=frame.copy()
+    frameHeight=frameOpencvDnn.shape[0]
+    frameWidth=frameOpencvDnn.shape[1]
+    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections=net.forward()
+    faceBoxes=[]
+    for i in range(detections.shape[2]):
+        confidence=detections[0,0,i,2]
+        if confidence>conf_threshold:
+            x1=int(detections[0,0,i,3]*frameWidth)
+            y1=int(detections[0,0,i,4]*frameHeight)
+            x2=int(detections[0,0,i,5]*frameWidth)
+            y2=int(detections[0,0,i,6]*frameHeight)
+            faceBoxes.append([x1,y1,x2,y2])
+            cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn,faceBoxes
+
+
+parser=argparse.ArgumentParser()
+parser.add_argument('-i')
+
+args=parser.parse_args()
+
+faceProto="opencv_face_detector.pbtxt"
+faceModel="opencv_face_detector_uint8.pb"
+ageProto="age_deploy.prototxt"
+ageModel="age_net.caffemodel"
+genderProto="gender_deploy.prototxt"
+genderModel="gender_net.caffemodel"
+
+MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList=['Male','Female']
+
+faceNet=cv2.dnn.readNet(faceModel,faceProto)
+ageNet=cv2.dnn.readNet(ageModel,ageProto)
+genderNet=cv2.dnn.readNet(genderModel,genderProto)
+
+video=cv2.VideoCapture(args.i if args.i else 0)
+padding=20
+g=''
+a=''
+while cv2.waitKey(1)<0:
+    hasFrame,frame=video.read()
+    if not hasFrame:
+        cv2.waitKey()
+        break
+
+    resultImg,faceBoxes=highlightFace(faceNet,frame)
+    if not faceBoxes:
+        print("No face detected")
+
+    for faceBox in faceBoxes:
+        face=frame[max(0,faceBox[1]-padding):
+                   min(faceBox[3]+padding,frame.shape[0]-1),max(0,faceBox[0]-padding)
+                   :min(faceBox[2]+padding, frame.shape[1]-1)]
+
+        blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds=genderNet.forward()
+        gender=genderList[genderPreds[0].argmax()]
+        print(f'Gender: {gender}')
+        g=gender
+
+        ageNet.setInput(blob)
+        agePreds=ageNet.forward()
+        age=ageList[agePreds[0].argmax()]
+        print(f'Age: {age[1:-1]} years')
+        a=age
+
+        cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
+        cv2.imshow("Detecting age and gender", resultImg)
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+if g=='Male':
+    model = tensorflow.keras.models.load_model('male.h5')
+else:
+    model = tensorflow.keras.models.load_model('keras_model.h5')
+    
+
+
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1.
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+print(args.i)
+# Replace this with the path to your image
+image = Image.open(args.i)
+
+#resize the image to a 224x224 with the same strategy as in TM2:
+#resizing the image to be at least 224x224 and then cropping from the center
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+#turn the image into a numpy array
+image_array = np.asarray(image)
+
+# display the resized image
+image.show()
+
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+# Load the image into the array
+data[0] = normalized_image_array
+
+# run the inference
+prediction = model.predict(data)
+#print(prediction.shape)
+#print(type(prediction))
+#print(prediction)
+result=0
+max=np.amax(prediction)
+if g=='Male':
+    name=['Shirt','Pant','Tshirt','Suit','Kurta','Shirt and Pant','Tshirt and pant','Tshirt and halfpant']
+else:
+    name=['Skirt','Saree','Top','Jeans','Salwar Suit','Top and Skirt','Gown']
+for x in prediction:
+    for y in x:
+        if y == max:
+            break
+        else:
+            result+=1
+#print(max)
+print(f'Gender: {g}')
+print(f'Age: {a[1:-1]} years')
+print(f'Wearing: {name[result]}')
+
